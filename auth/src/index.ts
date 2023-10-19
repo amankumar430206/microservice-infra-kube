57d899efc90b1bc6484bea89e67d3df5c1779045
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-10-19 10:35:57 +0530

    adding auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,23 @@ const app = express();
 
 app.use(json());
 
+const BASE_URL = "/api/users"
+
+app.get("/",(req,res)=>{
+    res.json({
+        msg : "welcome to KubeCtl Env"
+    })
+})
+
+app.get(BASE_URL + "/currentuser",(req,res)=>{
+    res.json({
+        name : "Aman Test"
+    })
+})
+
 const PORT = 3000;
 app.listen(PORT, ()=>{
-    console.log("Auth Service Started :: ", PORT)
+    console.log("Auth Service Started On The PORT :: ", PORT)
 })
 
 

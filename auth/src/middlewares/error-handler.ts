42f616c560c7d2926f2ec79736510e4716050a53
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-01 15:27:25 +0530

    error handler middleware

---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,13 @@
+import { Errback, NextFunction, Request, Response } from "express";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("something went wrong", err);
+  res.status(400).json({
+    error: err.message,
+  });
+};

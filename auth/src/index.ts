f6813b98a67aa790dc365c6420c67ff96a367621
Aman Vishwakarma <aman.vishwakarma.dev@gmail.com>
2023-11-01 15:27:12 +0530

    auth routes setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,27 +1,19 @@
 import express from "express";
-import {json} from "body-parser"
+import { json } from "body-parser";
+import { BASE_URL } from "./const";
+import { AppRoutes } from "./routes/appRoutes";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 
+const APP_BASE = BASE_URL;
 app.use(json());
 
-const BASE_URL = "/api/users"
-
-app.get("/",(req,res)=>{
-    res.json({
-        msg : "welcome to KubeCtl Env"
-    })
-})
-
-app.get(BASE_URL + "/currentuser",(req,res)=>{
-    res.json({
-        name : "Aman Test"
-    })
-})
+// router init
+app.use(APP_BASE, AppRoutes);
+app.use(errorHandler);
 
 const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log("Auth Service Started On The PORT :: ", PORT)
-})
-
-
+app.listen(PORT, () => {
+  console.log("Auth Service Started On The PORT :: ", PORT);
+});
